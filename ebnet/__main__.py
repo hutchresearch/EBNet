6ebd7c3a46df495b93045d8e83f1868750c5ac37
Logan Sizemore <sizemore0125@gmail.com>
2025-07-23 13:40:47 -0700

    adding notebook

--- ebnet/__main__.py
+++ ebnet/__main__.py
@@ -4,52 +4,10 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import torch
 import os
-import io
-import tempfile
 import skeletonkey as sk
-from collections import OrderedDict
 from runner import Runner
 from denorm import denormalize_labels, denormalize_std
-
-def franken_load(load_path: str, chunks: int) -> OrderedDict:
-    """
-    Loads a PyTorch model from multiple binary files that were previously split.
-
-    Args:
-        load_path: str, The directory where the model chunks are located.
-        chunks: int, The number of model chunks to load.
-
-    Returns:
-        A ordered dictionary containing the PyTorch model state.
-    """
-
-    def load_member(load_path: str, file_out: io.BufferedReader, file_i: int) -> None:
-        """
-        Reads a single chunk of the model from disk and writes it to a buffer.
-
-        Args:
-            load_path: str, The directory where the model chunk files are located.
-            file_out: io.BufferedReader, The buffer where the model chunks are written.
-            file_i: int, The index of the model chunk to read.
-
-        """
-        load_name = os.path.join(load_path, f"model_chunk_{file_i}")
-        with open(load_name, "rb") as file_in:
-            file_out.write(file_in.read())
-
-    with tempfile.TemporaryDirectory() as tempdir:
-        # Create a temporary file to write the model chunks.
-        model_path = os.path.join(tempdir, "model.pt")
-        with open(model_path, "wb") as file_out:
-            # Load each model chunk and write it to the buffer.
-            for i in range(chunks):
-                load_member(load_path, file_out, i)
-        
-        # Load the PyTorch model from the buffer.
-        state_dict = torch.load(model_path, map_location=torch.device("cpu"))
-
-    return state_dict
-
+from chop_model import franken_load
 
 def print_results_as_csv(pred, std, targets):
     print(",".join([t + "_pred" for t in targets]), end=",")
